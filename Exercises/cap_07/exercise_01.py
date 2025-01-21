9914f260db27e792bf20540987ddc18a8b999521
Alan Notebook <alancesar3456@gmail.com>
2025-01-21 19:24:04 -0300

    adicionando o cap 07

---
+++ Exercises/cap_07/exercise_01.py
@@ -0,0 +1,21 @@
+# Suponha que estamos construindo um aplicativo de entrega de comida e queremos armazenar informações dos restaurantes.
+
+# Crie uma classe Restaurant em um arquivo chamado restaurants.py com os seguintes atributos:
+
+# .name do tipo str
+# .category do tipo str
+# .rating do tipo float
+# .delivery do tipo bool
+# Deixe esses valores vazios/padrão por enquanto.
+
+# Nada deve acontecer quando você executar o código, porque ainda não usamos essa classe. No próximo exercício, começaremos a usá-la!
+
+# solucao:
+
+class Restaurant:
+  name = ''
+  category = ''
+  rating = 0.0
+  delivery = True
+
+# Nada deve acontecer quando você executar o código, porque ainda não usamos essa classe. No próximo exercício, começaremos a usá-la!
