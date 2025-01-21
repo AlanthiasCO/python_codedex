9e1a54d74585281a71a54fffad8cc297558b3921
Alan Mathias <alancesar3456@gmail.com>
2025-01-21 00:12:48 -0300

    subindo exercicios do desafio cap. 03

---
+++ Exercises/cap_03/bonus_1.py
@@ -0,0 +1,10 @@
+weather = 'Sunny'
+humidity = 35
+
+if weather == 'Sunny':
+  if humidity < 60:
+    print('Let’s go to the beach! 🏖️')
+  else:
+    print('Hmmm, it’s a little humid for a beach day.')
+else:
+  print('It’s not sunny today... let’s try for another day.')
