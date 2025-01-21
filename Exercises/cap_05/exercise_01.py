f064b0ecb8e7186adf48ba5c6fba8caae089d4dd
Alan Notebook <alancesar3456@gmail.com>
2025-01-21 16:11:05 -0300

    adicionando cap.05

---
+++ Exercises/cap_05/exercise_01.py
@@ -0,0 +1,10 @@
+# Criar um  programa com a lista de coisas que você precisa da loja: 
+# '🥚 Eggs'
+#'🥑 Avocados'
+#'🍪 Cookies'
+#'🌶 Hot Pepper Jam'
+#'Blueberries'
+#'🥦 Broccoli'
+
+grocery = ['🥚 Eggs', '🥑 Avocados', '🍪 Cookies', '🌶 Hot Pepper Jam', 'Blueberries', '🥦 Broccoli']
+print(grocery)
